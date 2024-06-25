44b3e5fbff38f6e4cdeaff78e8b51b2df452f50b
ShubhamSawant411 <shubhamsawant@nimapinfotech.com>
2024-06-25 15:01:58 +0530

    second commit

--- src/app/green-text.directive.ts
+++ src/app/green-text.directive.ts
@@ -1,11 +1,12 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, Renderer2, inject } from '@angular/core';
 
 @Directive({
   selector: '[appGreenText]'
 })
 export class GreenTextDirective {
 
+  private hostElementRef = inject<ElementRef>(ElementRef);
   constructor(private el: ElementRef) {
-    el.nativeElement.style.color = 'rgb(0,255,150)';
+    this.hostElementRef.nativeElement.style.color = 'rgb(0,255,150)';
   }
 }
